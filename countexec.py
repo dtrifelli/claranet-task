ef78988d1ebcd34dbda2952420601b073fbce903
dtrifelli <dtrifelli@torkd.space>
2023-04-16 14:57:27 +0200

    Updated with comments

--- countexec.py
+++ countexec.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python3
+
 from sys import argv, exit
 from os import walk
 import re
 
+# Check for correct number of parameters.
 if len(argv) != 2:
     exit("Error! Incorrect number of parameters.")
 
@@ -9,14 +12,17 @@ path = str(argv[1])
 
 enum_scripts = dict()
 
+# Work on files recursively.
 for (dir_path, dir_names, file_names) in walk(path):
     for file in file_names:
         file_path = "{}/{}".format(dir_path, file)
         try:
             with open(file_path, 'r') as fh:
                 line = str()
+                # Check for lines until a non-empty line is found.
                 while not line:
                     line = fh.readline().strip()
+                # If the first non-empty line starts with a shebang ('#!'), add it to the dictionary and increase its counter.
                 if re.findall("^#!", line):
                     enum_scripts[line] = 1 if line not in enum_scripts else enum_scripts[line]+1
         except Exception as e:
--- strrep.py
+++ strrep.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+
 from sys import argv, exit
 from os import walk
 
+# Check for correct number of paramteres.
 if len(argv) != 4:
     exit("Error! Incorrect number of parameters.")
 
@@ -8,16 +11,20 @@ to_replace = str(argv[1])
 replacement = str(argv[2])
 dir_path = str(argv[3])
 
+# Work on files recursively.
 for (dir_path, dir_names, file_names) in walk(dir_path):
     for file in file_names:
         file_path = "{}\{}".format(dir_path, file)
 
         try:
+            # Open file and read contents.
             with open(file_path, 'r') as fh:
                 file_data = fh.read()
         
+            # Replace string in file content.
             new_data = file_data.replace(to_replace, replacement)
 
+            # Open file again and replace content with the edited one.
             with open(file_path, 'w') as fh:
                 fh.write(new_data)
 
