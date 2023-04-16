0e2ea599b8f6ea3ea5c74c6399b565e6211781d8
dtrifelli <129297315+dtrifelli@users.noreply.github.com>
2023-04-16 14:59:32 +0200

    Typos

--- terraform-ec2-RDS-wordpress-master/main.tf
+++ terraform-ec2-RDS-wordpress-master/main.tf
@@ -3,17 +3,17 @@
 module aws_wordpress {
     source              = "./modules/latest"
     database_name           = "wordpress_db"   // database name
-    database_user           = "wordpress_user" //database username
-    // Password here will be used to create master db user.It should be chnaged later
-    database_password = "PassWord4-user" //password for user database
+    database_user           = "wordpress_user" // database username
+    // Password here will be used to create master db user.It should be changed later
+    database_password = "PassWord4-user" // password for user database
     wp_version = "latest" // WordPress version to be installed
-    shared_credentials_file = "~/.aws/credentials"         //Access key and Secret key file location
+    shared_credentials_file = "~/.aws/credentials"         // Access key and Secret key file location
     region                  = "eu-central-1" // Europe-Frankfurt region
-    IsUbuntu                = true             // true for ubuntu,false for linux 2  //boolean type
+    IsUbuntu                = true             // true for ubuntu, false for linux 2  //boolean type
     // avaibility zone and their CIDR
     AZ1          = "eu-central-1a" // for EC2
-    AZ2          = "eu-central-1b" //for RDS 
-    AZ3          = "eu-central-1c" //for RDS
+    AZ2          = "eu-central-1b" // for RDS 
+    AZ3          = "eu-central-1c" // for RDS
     VPC_cidr     = "10.0.0.0/16"     // VPC CIDR
     subnet1_cidr = "10.0.1.0/24"     // Public Subnet for EC2
     subnet2_cidr = "10.0.2.0/24"     // Private Subnet for RDS
